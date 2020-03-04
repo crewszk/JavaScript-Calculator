b19baae8d4f396a43bfad0af2283c582880e6efd
crewszk <crewszk@outlook.com>
2020-03-04 01:30:34 -0500

    (#4 replaced getElementId in favor of querySelector)

--- scripts/index.js
+++ scripts/index.js
@@ -1,76 +1,83 @@
-
+let parenthesis = false;
+let parenthesisCount = 0;
 
 //This segment deals with the first row of buttons on the calculator for clicking
-document.getElementById("left-parenthesis").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#left-parenthesis").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
+    parenthesis = true;
 });
-document.getElementById("right-parenthesis").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#right-parenthesis").addEventListener('click', function(event) {
+    if (parenthsis === true) {
+        document.querySelector("#input").value+=this.value;
+        if (--count === 0) {
+            parenthesis = false;
+        }
+    }
 });
-document.getElementById("modulo").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#clear").addEventListener('click', function(event) {
+    document.querySelector("#input").value='';
 });
-document.getElementById("clear").addEventListener('click', function(event) {
-    document.getElementById("input").value='';
+document.querySelector("#backspace").addEventListener('click', function(event) {
+    document.querySelector("#input").value=document.querySelector("#input").value.substring(0, document.querySelector("#input").value - 1);
 });
 
 //This segment deals with the second row of buttons on the calculator for clicking
-document.getElementById("seven").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#seven").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
-document.getElementById("eight").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#eight").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
-document.getElementById("nine").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#nine").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
-document.getElementById("divide").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#divide").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
 
 //This segment deals with the third row of buttons on the calculator for clicking
-document.getElementById("four").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#four").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
-document.getElementById("five").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#five").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
-document.getElementById("six").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#six").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
-document.getElementById("multiply").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#multiply").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
 
 //This segment deals with the fourth row of buttons on the calculator for clicking
-document.getElementById("one").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#one").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
-document.getElementById("two").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#two").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
-document.getElementById("three").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#three").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
-document.getElementById("subtract").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#subtract").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
 
 //This segment deals with the fifth row of buttons on the calculator for clicking
-document.getElementById("zero").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#zero").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
-document.getElementById("period").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#period").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
-document.getElementById("equals").addEventListener('click', function(event) {
-    Compute(document.getElementById("input").value);
+document.querySelector("#equals").addEventListener('click', function(event) {
+    Compute(document.querySelector("#input").value);
 });
-document.getElementById("add").addEventListener('click', function(event) {
-    document.getElementById("input").value+=this.value;
+document.querySelector("#add").addEventListener('click', function(event) {
+    document.querySelector("#input").value+=this.value;
 });
 
 //Function for computing the actual output
 function Compute(input) {
-    document.getElementById('input').value=eval(document.getElementById('input').value);
+    document.querySelector('input').value=eval(document.querySelector('input').value);
 }
