5f7fa5e7267d7c12f683eb9a807cdd5211e700dd
crewszk <crewszk@outlook.com>
2020-03-04 19:30:53 -0500

    (#6 Added period checking for illogical input)

--- scripts/operations.js
+++ scripts/operations.js
@@ -1,13 +1,14 @@
 let remainder = false;
+let period = false;
+let parenthesisCount = 0;
 
 /*This segment deals with the first row of buttons on the calculator for clicking
 
 This is the left-parenthesis button and it will keep track if there are left-parenthesis or not as it would be illogical
 to have a right parenthesis without a cooresponding left one.*/
-let parenthesisCount = 0;
 document.querySelector("#left-parenthesis").addEventListener('click', function(event) {
     remainder = false;
-    document.querySelector("#input").value+=this.value;
+    document.querySelector("#input").value += this.value;
     parenthesisCount++;
 });
 
@@ -17,21 +18,21 @@ document.querySelector("#right-parenthesis").addEventListener('click', function(
     remainder = false;    
     if (parenthesisCount > 0) {
         parenthesisCount--;
-        document.querySelector("#input").value+=this.value;
+        document.querySelector("#input").value += this.value;
     }
 });
 
 //This is the all clear button and it will completely clear the #input text field
 document.querySelector("#clear").addEventListener('click', function(event) {
     remainder = false;
-    document.querySelector("#input").value='';
+    document.querySelector("#input").value = '';
 });
 
 //This is the backspace button and it will make the #input text field equal a substring of the current value of the text field without
 //the last character
 document.querySelector("#backspace").addEventListener('click', function(event) {
     remainder = false;   
-    document.querySelector("#input").value=document.querySelector("#input").value.substring(0, document.querySelector("#input").value.length - 1);
+    document.querySelector("#input").value = document.querySelector("#input").value.substring(0, document.querySelector("#input").value.length - 1);
 });
 
 //This segment deals with the second row of buttons on the calculator for clicking
@@ -81,20 +82,17 @@ document.querySelector("#zero").addEventListener('click', function(event) {
     addNumber(this.value);
 });
 document.querySelector("#period").addEventListener('click', function(event) {
-    if (remainder === true) { 
-        document.querySelector("#input").value='';
-        remainder = false;
-    }
-        document.querySelector("#input").value+='0' + this.value;
+    addNumber(this.value);
 });
 
 //This is the equals button and it will activate the compute() method where it
 document.querySelector("#equals").addEventListener('click', function(event) {
     compute();
     remainder = true;
+    period = false;
 });
 document.querySelector("#add").addEventListener('click', function(event) {
-    document.querySelector("#input").value+=this.value;
+    addOperator(this.value);
 });
 
 //Function for computing the actual output
@@ -102,15 +100,27 @@ function compute() {
     document.querySelector('input').value=eval(document.querySelector('input').value);
 }
 
+//Encapsulation of applying a number to the text field
 function addNumber(number) {
     if (remainder === true) { 
-        document.querySelector("#input").value='';
+        document.querySelector("#input").value = '';
         remainder = false;
     }
-        document.querySelector("#input").value+=number;
+    if (number === '.'){
+        if (period === false) {
+            document.querySelector("#input").value += '0';
+            period = true;
+        }
+        else {
+            return;
+        }
+    }
+    document.querySelector("#input").value += number;
 }
 
+//Encapsulation of applying an operator to the text field
 function addOperator(operator) {
-    remainder = false;   
-    document.querySelector("#input").value+=operator;
+    remainder = false;
+    period = false;
+    document.querySelector("#input").value += operator;
 }
